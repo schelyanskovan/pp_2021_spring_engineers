d037751c595f6bb1326b198a30f2453497de7fd4
Denis Kadnikov <denchikdreams@gmail.com>
2021-04-05 23:31:34 +0300

    fix code style

--- modules/task_1/kadnikov_d_linear_filter_vert/linear_filter_vert.h
+++ modules/task_1/kadnikov_d_linear_filter_vert/linear_filter_vert.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Kadnikov Denis
-#ifndef MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_H_
-#define MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_H_
+#ifndef MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_LINEAR_FILTER_VERT_H_
+#define MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_LINEAR_FILTER_VERT_H_
 
 #include <ctime>
 #include <random>
@@ -23,4 +23,4 @@ std::vector<double> gaussianCalculate(int radius, int sigma);
 std::vector<double> createRandomVector(int r, int c);
 std::vector<double> transposition(const std::vector<double>& vec, int r, int c);
 
-#endif  // MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_H_
+#endif  // MODULES_TASK_1_KADNIKOV_D_LINEAR_FILTER_VERT_LINEAR_FILTER_VERT_H_
