c4fbf9d8f1d6258005ce36cb704de557d7ae3d8b
OganyanRV <OgRobertino2@gmail.com>
2021-04-16 01:23:47 +0300

    finally working stupid program

--- modules/Visualize/visualize.cpp
+++ modules/Visualize/visualize.cpp
@@ -27,15 +27,16 @@ void convert_to_zeroone(cv::Mat *img) {
 }
 
 
-void convert_to_rdm_color(std::vector<cv::Vec3w> *img) {
-    std::default_random_engine generator;
+void convert_to_rdm_color(std::vector<cv::Vec3b> *img) {
+    std::random_device rd;
+    std::default_random_engine generator(rd());
     std::uniform_int_distribution<unsigned int> distribution(0, 255);
-    std::map<uint, cv::Vec3w> colors;
+    std::map<uint, cv::Vec3b> colors;
     for (uint64_t i = 0; i < (*img).size(); ++i) {
         int cur_num = (*img)[i][0];
         if (cur_num > 1) {
             if (colors.find(cur_num) == colors.end()) {
-                cv::Vec3w result;
+                cv::Vec3b result;
                 for (int j = 0; j < 3; ++j) {
                     result[j] = distribution(generator);
                 }
@@ -44,6 +45,8 @@ void convert_to_rdm_color(std::vector<cv::Vec3w> *img) {
             } else {
                 (*img)[i] = colors[cur_num];
             }
+        } else {
+            (*img)[i] = {255, 255, 255};
         }
     }
     return;
@@ -57,30 +60,21 @@ int main(int argc, char *argv[]) {
 
     if (argc < 3) {
         std::cout << "Wrong input" << "\n";
+        return 0;
     }
 
     cv::Mat img = imread(argv[1], cv::IMREAD_GRAYSCALE);
-    cv::imwrite("/home/ogrob/pp_2021_spring_engineers/modules/Visualize/results/loh2.png", img);
 
-    //  std::vector<cv::Vec3b> array(img.datastart, img.dataend);
-    //  std::vector<uint16_t> before_convert_array(img.datastart, img.dataend);
+    if (img.empty()) {
+        std::cout << "Wrong data" << "\n";
+        return 0;
+    }
     convert_to_zeroone(&img);
     std::vector<uint16_t> array(img.datastart, img.dataend);
     std::pair<std::vector<uint16_t>, uint16_t> new_img = MarkComponents(array, img.rows, img.cols);
-    cv::Mat kek(img.rows, img.cols, CV_8U, new_img.first.data());
-    cv::imwrite("/home/ogrob/pp_2021_spring_engineers/modules/Visualize/results/loh.png", kek);
-    std::vector<cv::Vec3w> result_image(new_img.first.begin(), new_img.first.end());
-    std::vector<uchar> array2(img.datastart, img.dataend);
+    std::vector<cv::Vec3b> result_image(new_img.first.begin(), new_img.first.end());
     convert_to_rdm_color(&result_image);
-    //   cv::Mat new_img(array, img.rows, img.cols);
     cv::Mat final_img(img.rows, img.cols, CV_8UC3, result_image.data());
-
-    for (uint64_t i = 0; i < (result_image).size(); ++i) {
-        for (int j = 0; j < 3; ++j) {
-            std::cout << result_image[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
     cv::imwrite(argv[2], final_img, {cv::IMWRITE_JPEG_QUALITY});
 
     return 0;
