44b0e9bdfbcd0f35053e62fffc647e0c55c34b85
schelyanskovan <super.nastya21-08@yandex.ru>
2021-04-18 16:17:10 +0300

    done

---
+++ modules/task_2/schelyanskova_a_radix_sort_omp/radix_sort_omp.h
@@ -0,0 +1,20 @@
+// Copyright 2021 Schelyanskova Anastasiia
+#ifndef MODULES_TASK_2_SCHELYANSKOVA_A_RADIX_SORT_OMP_RADIX_SORT_OMP_H_
+#define MODULES_TASK_2_SCHELYANSKOVA_A_RADIX_SORT_OMP_RADIX_SORT_OMP_H_
+
+#include <string>
+#include <vector>
+#include <omp.h>
+
+std::vector<int> getRandomVector(int size);
+int get_max_power(std::vector<int> vect);
+bool vector_sort(std::vector<int> vect);
+std::vector<int> RadixSort(std::vector<int> vect, int size);
+std::vector<int> Merge(const std::vector<int> vect_left,
+                       const std::vector<int> vect_right);
+std::vector<int> RadixSortParallel(std::vector<int> vect, int size,
+                                   int threads);
+std::vector<int> MergeOmp(std::vector<int> vect_left,
+                          std::vector<int> vect_right);
+
+#endif  // MODULES_TASK_2_SCHELYANSKOVA_A_RADIX_SORT_OMP_RADIX_SORT_OMP_H_
