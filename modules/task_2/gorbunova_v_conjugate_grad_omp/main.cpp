3ca7c39c99d6b102f14035bb04c543126a815dea
Valeriya Gorbunova <47732103+GorbunovaVI@users.noreply.github.com>
2021-04-20 01:18:29 +0300

    Update main.cpp

--- modules/task_2/gorbunova_v_conjugate_grad_omp/main.cpp
+++ modules/task_2/gorbunova_v_conjugate_grad_omp/main.cpp
@@ -4,6 +4,17 @@
 #include <omp.h>
 #include "./conjugate_gradient.h"
 
+TEST(Conj_Grad, test_2_Rand_Matrix_10x10_10_proc) {
+    int S = 10;
+    double** A = new double* [S];
+    for (int count = 0; count < S; count++)
+        A[count] = new double[S];
+    Random_Matrix_A(S, A);
+    double* B = new double[S];
+    Random_Matrix_B(S, B);
+    ASSERT_EQ(0, conj_grad(A, B, S, 10));
+}
+
 TEST(Conj_Grad, test_1_Rand_Matrix_5x5_5_proc) {
     int S = 5;
     double** A = new double* [S];
@@ -20,18 +31,6 @@ TEST(Conj_Grad, test_1_Rand_Matrix_5x5_5_proc) {
     }
     ASSERT_EQ(0, conj_grad(A, B, S, 3));
 }
-
-TEST(Conj_Grad, test_2_Rand_Matrix_10x10_10_proc) {
-    int S = 10;
-    double** A = new double* [S];
-    for (int count = 0; count < S; count++)
-        A[count] = new double[S];
-    Random_Matrix_A(S, A);
-    double* B = new double[S];
-    Random_Matrix_B(S, B);
-    ASSERT_EQ(0, conj_grad(A, B, S, 10));
-}
-
 
 TEST(Conj_Grad, test_3_Rand_Matrix_50x50_4_proc) {
     int S = 50;
