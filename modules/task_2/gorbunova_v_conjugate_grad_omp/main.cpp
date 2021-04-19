dccc199a47089352e74623d9987e2ca1e6523eed
Valeriya Gorbunova <47732103+GorbunovaVI@users.noreply.github.com>
2021-04-20 01:08:50 +0300

    Update main.cpp

--- modules/task_2/gorbunova_v_conjugate_grad_omp/main.cpp
+++ modules/task_2/gorbunova_v_conjugate_grad_omp/main.cpp
@@ -21,18 +21,7 @@ TEST(Conj_Grad, test_1_Rand_Matrix_5x5_5_proc) {
     ASSERT_EQ(0, conj_grad(A, B, S, 3));
 }
 
-TEST(Conj_Grad, test_2_Rand_Matrix_10x10_6_proc) {
-    int S = 10;
-    double** A = new double* [S];
-    for (int count = 0; count < S; count++)
-        A[count] = new double[S];
-    Random_Matrix_A(S, A);
-    double* B = new double[S];
-    Random_Matrix_B(S, B);
-    ASSERT_EQ(0, conj_grad(A, B, S, 6));
-}
-
-TEST(Conj_Grad, test_3_Rand_Matrix_10x10_10_proc) {
+TEST(Conj_Grad, test_2_Rand_Matrix_10x10_10_proc) {
     int S = 10;
     double** A = new double* [S];
     for (int count = 0; count < S; count++)
@@ -44,7 +33,7 @@ TEST(Conj_Grad, test_3_Rand_Matrix_10x10_10_proc) {
 }
 
 
-TEST(Conj_Grad, test_4_Rand_Matrix_50x50_4_proc) {
+TEST(Conj_Grad, test_3_Rand_Matrix_50x50_4_proc) {
     int S = 50;
     double** A = new double* [S];
     for (int count = 0; count < S; count++)
@@ -55,7 +44,7 @@ TEST(Conj_Grad, test_4_Rand_Matrix_50x50_4_proc) {
     ASSERT_EQ(0, conj_grad(A, B, S, 4));
 }
 
-TEST(Conj_Grad, test_5_Rand_Matrix_50x50_9_proc) {
+TEST(Conj_Grad, test_4_Rand_Matrix_50x50_9_proc) {
     int S = 50;
     double** A = new double* [S];
     for (int count = 0; count < S; count++)
@@ -66,7 +55,7 @@ TEST(Conj_Grad, test_5_Rand_Matrix_50x50_9_proc) {
     ASSERT_EQ(0, conj_grad(A, B, S, 9));
 }
 
-TEST(Conj_Grad, test_6_Rand_Matrix_100x100_13_proc) {
+TEST(Conj_Grad, test_5_Rand_Matrix_100x100_13_proc) {
     int S = 100;
     double** A = new double* [S];
     for (int count = 0; count < S; count++)
