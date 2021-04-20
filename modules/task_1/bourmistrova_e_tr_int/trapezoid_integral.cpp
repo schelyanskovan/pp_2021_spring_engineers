0400358d4bdbda0249a57e7a3a4833837d6411a8
BurmistrovaEO <tigrettokat@mail.ru>
2021-04-20 21:33:38 +0300

    Sequential Trapezoid Integral on new branch

--- modules/task_1/bourmistrova_e_tr_int/trapezoid_integral.cpp
+++ modules/task_1/bourmistrova_e_tr_int/trapezoid_integral.cpp
@@ -14,48 +14,46 @@ double CheckCoeff(int i, const std::pair<int, int>& pair) {
         qju *= 0.5;
     return qju;
 }
+
+void SetPrecisionVariables(double &n, double &m, double &k) {
+	std::cout << "Input Precision Variable For Outermost Sum: ";
+	std::cin >> n >> m >> k;
+}
 double SolveSequential(const std::vector<std::pair<int, int>>& bord,
-    std::function<double(double, double, double)> f) {
+    std::function<double(double, double, double)> f) {   // add n,m,k;
     double sum = 0;
     int i = 0, j = 0, k = 0;
     if (bord.size() == 1) {  // ONE DIMENSION
-        // int h = (bord[0].second.second
-        // - bord[0].second.first) / num_threads;
+		////double h = (bord[0].second - bord[0].first) / n;
         for (i = bord[0].first; i < bord[0].second; i++) {
-            sum += ((f(i, 1, 1) + f(i + 1, 1, 1)) / 2);
+            sum += ((f(i, 1, 1) + f(i + 1, 1, 1)) / 2); // i + h   и умножить на h
         }
     } else if (bord.size() == 2) {  // TWO DIMENSIONS
         double q0 = 1, q1 = 1;
-        // int h1 = (bord[0].second.second
-        // - bord[0].second.first) / num_threads;
-        // int h2 = (bord[1].second.second
-        // - bord[1].second.first) / num_threads;
-        for (i = bord[0].first; i < bord[0].second+1; i++) {
-            // q = 1;
+        // double h1 = (bord[0].second - bord[0].first) / n;
+        // double h2 = (bord[1].second - bord[1].first) / k;
+        for (i = bord[0].first; i < bord[0].second+1; i++) { // с шагом n
             q0 = CheckCoeff(i, bord[0]);
-            for (j = bord[1].first; j < bord[1].second+1; j++) {
+            for (j = bord[1].first; j < bord[1].second+1; j++) { // с шагом m
                     q1 = CheckCoeff(j, bord[1]);
-                    sum += q0 * q1 * f(i, j, 1);
+                    sum += q0 * q1 * f(i, j, 1); 
             }
-        }
+        } // домножить на h1, h2
     } else {  // THREE DIMENSIONS
         double q0 = 1, q1 = 1, q2 = 1;
-        // int h1 = (bord[0].second.second
-        // - bord[0].second.first) / num_threads;
-        // int h2 = (bord[1].second.second
-        // - bord[1].second.first) / num_threads;
-        // int h3 = (bord[2].second.second
-        // - bord[2].second.first) / num_threads;
-        for (i = bord[0].first; i < bord[0].second + 1; i++) {
+        // int h1 = (bord[0].second - bord[0].first) / n;
+        // int h2 = (bord[1].second - bord[1].first) / m;
+        // int h3 = (bord[2].second - bord[2].first) / k;
+        for (i = bord[0].first; i < bord[0].second + 1; i++) { // с шагом n
             q0 = CheckCoeff(i, bord[0]);
-            for (j = bord[1].first; j < bord[1].second + 1; j++) {
+            for (j = bord[1].first; j < bord[1].second + 1; j++) { // с шагом m
                 q1 = CheckCoeff(j, bord[1]);
-                for (k = bord[2].first; k < bord[2].second + 1; k++) {
+                for (k = bord[2].first; k < bord[2].second + 1; k++) { // с шагом k
                         q2 = CheckCoeff(k, bord[2]);
-                        sum += q0 * q1 * q2 * f(i, j, k);
+                        sum += q0 * q1 * q2 * f(i, j, k);  
                 }
             }
-        }
+        } // домножить на h1, h2, h3
     }
     return sum;
 }
