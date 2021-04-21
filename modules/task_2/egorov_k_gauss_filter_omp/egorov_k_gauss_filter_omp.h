1cbcda2279aabe82999cecd0520eec16acd09efc
kegorov902 <kegorov62@gmail.com>
2021-04-21 22:37:57 +0300

    commit #1 - first commit for saving

---
+++ modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.h
@@ -0,0 +1,18 @@
+// Copyright 2018 Nesterov Alexander
+#ifndef MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+#define MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+
+#include <vector>
+#include <string>
+
+std::vector<int> getRandomVector(int  sz);
+int clamp(int value, int min, int max);
+std::vector<float> calculateKernel3x3(float sigma);
+int pColourCalculation(const std::vector<int> &img, int w, int h,
+	int x, int y, std::vector<float> kernel);
+std::vector<int> filterApply(const std::vector<int> &img,
+	int w, int h, float sigma);
+std::vector<int> filterApplyParallel(const std::vector<int> &img,
+	int w, int h, float sigma);
+
+#endif  // MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
