6d51b5cd3fd5004077d821becb3294e95418e3a9
kegorov902 <kegorov62@gmail.com>
2021-04-21 23:46:58 +0300

    commit #2 - fixed cpplint issues, fixed time comparison

--- modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.cpp
+++ modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Nesterov Alexander
+// Copyright 2020 Egorov Kirill
 #include <omp.h>
 #include <vector>
 #include <string>
@@ -6,91 +6,91 @@
 #include <iostream>
 #include "../../../modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.h"
 
-std::vector<int> getRandomVector(int  sz) {
+std::vector<int> getRandomVector(int sz) {
     std::random_device dev;
     std::mt19937 gen(dev());
     std::vector<int> vec(sz);
-    for (int  i = 0; i < sz; i++) { vec[i] = gen() % 100; }
+    for (int i = 0; i < sz; i++) { vec[i] = gen() % 100; }
     return vec;
 }
 
 int clamp(int value, int min, int max) {
-	if (value < min)
-		return min;
-	else if (value > max)
-		return max;
-	else
-		return value;
+    if (value < min)
+        return min;
+    else if (value > max)
+        return max;
+    else
+        return value;
 }
 
 std::vector<float> calculateKernel3x3(float sigma) {
-	std::vector<float> kernel(9);  // 3*3 kernel vector
-	float n = 0;
+    std::vector<float> kernel(9);  // 3*3 kernel vector
+    float n = 0;
 
-	for (int i = -1; i <= 1; i++) {
-		for (int j = -1; j <= 1; j++) {
-			kernel[(i + 1) * 3 + j + 1] = (exp(-(i * i + j * j)
-				/ (2 * sigma * sigma)));
-			n = n + kernel[(i + 1) * 3 + j + 1];
-		}
-	}
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            kernel[(i + 1) * 3 + j + 1] = (exp(-(i * i + j * j)
+                                          / (2 * sigma * sigma)));
+            n = n + kernel[(i + 1) * 3 + j + 1];
+        }
+    }
 
-	for (int i = 0; i < 9; i++) {
-		kernel[i] = kernel[i] / n;
-	}
+    for (int i = 0; i < 9; i++) {
+        kernel[i] = kernel[i] / n;
+    }
 
-	return kernel;
+    return kernel;
 }
 
 int pColourCalculation(const std::vector<int> &img, int w, int h,
-	int x, int y, std::vector<float> kernel) {
-	int nColour = 0;
-	float sColour = 0;
-	for (int i = -1; i <= 1; i++) {
-		for (int j = -1; j <= 1; j++) {
-			x = clamp(x + j, 0, w - 1);
-			y = clamp(y + i, 0, h - 1);
-			nColour = img[x * w + y];
-			sColour += nColour * kernel[(j + 1) * 3 + i + 1];
-		}
-	}
+                       int x, int y, std::vector<float> kernel) {
+    int nColour = 0;
+    float sColour = 0;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            x = clamp(x + j, 0, w - 1);
+            y = clamp(y + i, 0, h - 1);
+            nColour = img[x * w + y];
+            sColour += nColour * kernel[(j + 1) * 3 + i + 1];
+        }
+    }
 
-	return clamp(static_cast<int>(sColour), 0, 255);
+    return clamp(static_cast<int>(sColour), 0, 255);
 }
 
 std::vector<int> filterApply(const std::vector<int> &img,
-	int w, int h, float sigma) {
-	std::vector<int> res(w * h);
-	std::vector<float> kernel(9);
+    int w, int h, float sigma) {
+    std::vector<int> res(w * h);
+    std::vector<float> kernel(9);
 
-	kernel = calculateKernel3x3(sigma);
+    kernel = calculateKernel3x3(sigma);
 
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-			res[i*w + j] = pColourCalculation(img, w, h, i, j, kernel);
-		}
-	}
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            res[i*w + j] = pColourCalculation(img, w, h, i, j, kernel);
+        }
+    }
 
-	return res;
+    return res;
 }
 
 std::vector<int> filterApplyParallel(const std::vector<int> &img,
-	int w, int h, float sigma) {
-	std::vector<int> res(w * h);
-	std::vector<float> kernel(9);
+    int w, int h, float sigma) {
+    std::vector<int> res(w * h);
+    std::vector<float> kernel(9);
 
-	kernel = calculateKernel3x3(sigma);
+    kernel = calculateKernel3x3(sigma);
 
-	int i = 0, j = 0;
+    int i = 0, j = 0;
 
-	omp_set_dynamic(16);
+    omp_set_dynamic(16);
 
 #pragma omp parallel for private(i, j) shared(img, w, h, kernel, res)
-	for (i = 0; i < w; i++) {
-		for (j = 0; j < h; j++) {
-			res[i*w + j] = pColourCalculation(img, w, h, i, j, kernel);
-		}
-	}
+    for (i = 0; i < w; i++) {
+        for (j = 0; j < h; j++) {
+            res[i * w + j] = pColourCalculation(img, w, h, i, j, kernel);
+        }
+    }
 
-	return res;
+    return res;
 }
--- modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.h
+++ modules/task_2/egorov_k_gauss_filter_omp/egorov_k_gauss_filter_omp.h
@@ -1,6 +1,6 @@
-// Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
-#define MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+// Copyright 2020 Egorov Kirill
+#ifndef MODULES_TASK_2_EGOROV_K_GAUSS_FILTER_OMP_EGOROV_K_GAUSS_FILTER_OMP_H_
+#define MODULES_TASK_2_EGOROV_K_GAUSS_FILTER_OMP_EGOROV_K_GAUSS_FILTER_OMP_H_
 
 #include <vector>
 #include <string>
@@ -9,10 +9,10 @@ std::vector<int> getRandomVector(int  sz);
 int clamp(int value, int min, int max);
 std::vector<float> calculateKernel3x3(float sigma);
 int pColourCalculation(const std::vector<int> &img, int w, int h,
-	int x, int y, std::vector<float> kernel);
+    int x, int y, std::vector<float> kernel);
 std::vector<int> filterApply(const std::vector<int> &img,
-	int w, int h, float sigma);
+    int w, int h, float sigma);
 std::vector<int> filterApplyParallel(const std::vector<int> &img,
-	int w, int h, float sigma);
+    int w, int h, float sigma);
 
-#endif  // MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+#endif  // MODULES_TASK_2_EGOROV_K_GAUSS_FILTER_OMP_EGOROV_K_GAUSS_FILTER_OMP_H_
