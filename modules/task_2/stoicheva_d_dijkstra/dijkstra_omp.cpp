a440e39832aeafc06972829f4a46c5da45efbbf7
Darya <dstoicheva@mail.ru>
2021-05-07 23:44:10 +0300

    op

--- modules/task_2/stoicheva_d_dijkstra/dijkstra_omp.cpp
+++ modules/task_2/stoicheva_d_dijkstra/dijkstra_omp.cpp
@@ -3,57 +3,294 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <algorithm>
 #include <iostream>
+#include <sstream>
 #include "../../../modules/task_2/stoicheva_d_dijkstra/dijkstra_omp.h"
 
-std::vector<int> getRandomVector(int  sz) {
-    std::random_device dev;
-    std::mt19937 gen(dev());
-    std::vector<int> vec(sz);
-    for (int  i = 0; i < sz; i++) { vec[i] = gen() % 100; }
-    return vec;
+void print_graph(std::vector<int> graph, const size_t points, std::string prefix) {
+#ifdef DEBUG_PRINT
+    std::cout << "\n" << prefix << "Graph: {\n";
+    for (size_t row = 0; row < points; row++) {
+        std::cout << prefix << "  ";
+        for (size_t column = 0; column < points; column++) {
+            std::cout << graph[row * points + column] << ", ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << prefix << "}\n";
+#endif
 }
 
-int getParallelOperations(std::vector<int> vec, const std::string& ops) {
-    const int  sz = vec.size();
-    int reduction_elem = 1;
-    double start = omp_get_wtime();
-    if (ops == "+") {
-        #pragma omp parallel for reduction(+:reduction_elem)
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem += vec[i];
-        }
-    } else if (ops == "-") {
-        #pragma omp parallel for reduction(-:reduction_elem)
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem -= vec[i];
-        }
-    } else if (ops == "*") {
-        #pragma omp parallel for reduction(*:reduction_elem)
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem *= vec[i];
-        }
-    }
-    double finish = omp_get_wtime();
-    std::cout << "How measure time in OpenMP: " << finish - start << std::endl;
-    return reduction_elem;
+void print_vector(const std::vector<int> vector, const size_t size, const std::string prefix,
+    const std::string label, std::ostream& out) {
+    print_vector<int>(vector, size, prefix, label, out);
 }
 
-int getSequentialOperations(std::vector<int> vec, const std::string& ops) {
-    const int  sz = vec.size();
-    int reduction_elem = 1;
-    if (ops == "+") {
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem += vec[i];
-        }
-    } else if (ops == "-") {
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem -= vec[i];
-        }
-    } else if (ops == "*") {
-        for (int  i = 0; i < sz; i++) {
-            reduction_elem *= vec[i];
+
+void print_vector(const std::vector<float> vector, const size_t size, const std::string prefix,
+    const std::string label, std::ostream& out) {
+    print_vector<float>(vector, size, prefix, label, out);
+}
+
+
+void print_vector(const std::vector<double> vector, const size_t size, const std::string prefix,
+    const std::string label, std::ostream& out) {
+    print_vector<double>(vector, size, prefix, label, out);
+}
+
+
+template<typename T>
+void print_vector(const std::vector<T> vector, const size_t size, const std::string prefix,
+    const std::string label, std::ostream& out) {
+#ifdef DEBUG_PRINT
+    std::stringstream buffer;
+    buffer << prefix + (label.empty() ? "Vector" : label) + ": {" << std::endl;
+    buffer << prefix << "  ";
+    for (size_t i = 0; i < size; i++) {
+        buffer << vector[i] << ", ";
+    }
+    buffer << std::endl << prefix << "}" << std::endl;
+    out << buffer.str();
+#endif
+}
+
+int find_unprocessed_point_with_min_distance(const std::vector<int>& graph,
+    const std::vector<int>& distances, const std::vector<bool>& processed) {
+    int found_point = -1;
+    int found_min_distance = std::numeric_limits<int>::max();
+    for (size_t point = 0; point < processed.size(); point++) {
+        if (!processed[point] && distances[point] < found_min_distance) {
+            found_min_distance = distances[point];
+            found_point = static_cast<int>(point);
         }
     }
-    return reduction_elem;
+    // std::cout << "FUP: Next point with min distance " << found_min_distance
+    //           << " from " << 0 << " is " << found_point << std::endl;
+    return found_point;
+}
+
+int find_unprocessed_point_with_min_distance_omp(const std::vector<int>& graph,
+    const std::vector<int>& distances, const std::vector<bool>& processed) {
+    int found_point = -1;
+    int found_min_distance = std::numeric_limits<int>::max();
+    #pragma omp parallel
+    {
+        int local_found_point = found_point;
+        int local_found_min_distance = found_min_distance;
+        #pragma omp for
+        for (int point = 0; point < processed.size(); point++) {
+            if (!processed[point] && distances[point] < local_found_min_distance) {
+                local_found_min_distance = distances[point];
+                local_found_point = point;
+            }
+        }
+        #pragma omp critical
+        {
+            if (found_min_distance > local_found_min_distance) {
+                found_min_distance = local_found_min_distance;
+                found_point = local_found_point;
+            }
+        }
+    }
+    return found_point;
+}
+
+int process_unprocessed_point(const std::vector<int>& graph,
+    std::vector<int>* distances,
+    std::vector<bool>* processed, int current_point) {
+
+    int min_distance = std::numeric_limits<int>::max();
+    int min_distance_point = -1;
+    for (int point = 0; point < processed->size(); point++) {
+        int start_row_index = current_point * static_cast<int>(processed->size());
+        int distance = graph[start_row_index + point];
+        if (!(*processed)[point] && distance > 0) {
+            int *dp = distances->data() + point;
+            int *dcp = distances->data() + current_point;
+            *dp = std::min(*dp, *dcp + distance);
+            if (min_distance > *dp) {
+                min_distance = *dp;
+                min_distance_point = point;
+            }
+        }
+    }
+    (*processed)[current_point] = true;
+    // std::cout << "PUP: Next point with min distance " << min_distance << " is "
+    //           << min_distance_point + 1 << std::endl;
+    return min_distance_point;
+}
+
+int process_unprocessed_point_omp(const std::vector<int>& graph,
+    std::vector<int>* distances,
+    std::vector<bool>* processed, int current_point) {
+
+    int min_distance = std::numeric_limits<int>::max();
+    int min_distance_point = -1;
+    #pragma omp parallel
+    {
+        int local_min_distance = std::numeric_limits<int>::max();
+        int local_min_distance_point = -1;
+        #pragma omp for
+        for (int point = 0; point < processed->size(); point++) {
+            int start_row_index = current_point * static_cast<int>(processed->size());
+            int distance = graph[start_row_index + point];
+            if (!(*processed)[point] && distance > 0) {
+                int* dp = distances->data() + point;
+                int* dcp = distances->data() + current_point;
+                *dp = std::min(*dp, *dcp + distance);
+                if (local_min_distance > *dp) {
+                    local_min_distance = *dp;
+                    local_min_distance_point = point;
+                }
+            }
+        }
+        #pragma omp critical
+        {
+            if (min_distance > local_min_distance) {
+                min_distance = local_min_distance;
+                min_distance_point = local_min_distance_point;
+            }
+        }
+    }
+    (*processed)[current_point] = true;
+    return min_distance_point;
+}
+
+std::vector<int> dijkstra(const std::vector<int>& graph, int start, int end) {
+    if (graph.size() == 0) {
+        throw "Error: empty graph";
+    }
+
+    size_t points_count = static_cast<size_t>(sqrt(graph.size()));
+    if (points_count * points_count != static_cast<int>(graph.size())) {
+        std::cout << "Illegal size: expected " << graph.size() << " calculated "
+                  << points_count * points_count << std::endl;
+        throw "Error: incorrect graph";
+    }
+
+    start--; end--;
+    if (start < 0 || end < 0 || start > points_count - 1 || end > points_count - 1) {
+        std::cout << "Error: illegal start or end" << std::endl;
+        throw "Error: illegal start or end";
+    }
+
+    if (points_count == 1) {
+        return std::vector<int>({ start + 1 });
+    }
+
+    constexpr int max_int = std::numeric_limits<int>::max();
+    std::vector<int> distances = std::vector<int>(points_count, max_int);
+    std::vector<bool> processed = std::vector<bool>(points_count, false);
+
+    distances[start] = 0;
+
+    int next_unprocessed_point = start;
+    while (next_unprocessed_point >= 0) {
+        next_unprocessed_point = process_unprocessed_point(graph,
+            &distances, &processed, next_unprocessed_point);
+        if (next_unprocessed_point < 0) {
+            // std::cout << "- next unprocessed point not given, finding." << std::endl;
+            next_unprocessed_point =
+                find_unprocessed_point_with_min_distance(graph, distances,
+                    processed);
+        } else {
+            // std::cout << "+ next unprocessed point given, ok." << std::endl;
+        }
+    }
+    print_vector(distances, distances.size(), "distances");
+
+    std::vector<int> path;
+    path.push_back(end + 1);
+    int weight = distances[end];
+    int current = end;
+
+    while (current != start) {
+        for (int i = 0; i < points_count; i++) {
+            if (graph[current * points_count + i] > 0) {
+                int tmp = weight - graph[current * points_count + i];
+                if (distances[i] == tmp) {
+                    weight = tmp;
+                    current = i;
+                    path.insert(path.begin(), i + 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    return path;
+}
+
+std::vector<int> dijkstra_omp(const std::vector<int>& graph, int start, int end) {
+    if (graph.size() == 0)
+        throw "Error: empty graph";
+
+    size_t points_count = static_cast<size_t>(sqrt(graph.size()));
+
+    if (points_count * points_count != static_cast<int>(graph.size())) {
+        std::cout << "Illegal size: expected " << graph.size() << " calculated "
+                  << points_count * points_count << std::endl;
+        throw "Error: incorrect graph";
+    }
+
+    start--; end--;
+
+    if (start < 0 || end < 0 || start > points_count - 1 || end > points_count - 1) {
+        std::cout << "Error: illegal start or end" << std::endl;
+        throw "Error: illegal start or end";
+    }
+    if (points_count == 1) {
+        return std::vector<int>({ 0 });
+    }
+
+    constexpr int max_int = std::numeric_limits<int>::max();
+    std::vector<int> distances = std::vector<int>(points_count, max_int);
+    std::vector<bool> processed = std::vector<bool>(points_count, false);
+
+    distances[start] = 0;
+
+    int next_unprocessed_point = start;
+    while (next_unprocessed_point >= 0) {
+            next_unprocessed_point = process_unprocessed_point_omp(graph,
+                &distances, &processed, next_unprocessed_point);
+            if (next_unprocessed_point < 0) {
+                next_unprocessed_point =
+                    find_unprocessed_point_with_min_distance_omp(graph, distances,
+                        processed);
+            }
+    }
+    print_vector(distances, distances.size(), "distances:");
+
+    std::vector<int> path;
+    path.push_back(end + 1);
+    int weight = distances[end];
+    int current = end;
+    bool found = false;
+
+    while (current != start) {
+        found = false;
+
+        #pragma omp parallel for
+        for (int i = 0; i < points_count; i++) {
+            if (found) {
+                break;
+            }
+            if (graph[current * points_count + i] > 0) {
+                int tmp = weight - graph[current * points_count + i];
+                if (distances[i] == tmp) {
+                    #pragma omp critical
+                    {
+                        weight = tmp;
+                        current = i;
+                        path.insert(path.begin(), i + 1);
+                        found = true;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    return path;
 }
--- modules/task_2/stoicheva_d_dijkstra/main.cpp
+++ modules/task_2/stoicheva_d_dijkstra/main.cpp
@@ -1,44 +1,154 @@
-// Copyright 2018 Nesterov Alexander
+// Copyright 2021 Stoicheva Darya
 #include <gtest/gtest.h>
+#include <omp.h>
 #include <vector>
-#include "./stoicheva_d_dijkstra_omp.h"
+#include <iostream>
+#include "../../../modules/task_2/stoicheva_d_dijkstra/dijkstra_omp.h"
 
-TEST(Parallel_Operations_OpenMP, Test_Sum) {
-    std::vector<int> vec = getRandomVector(100);
-    int sequential_sum = getSequentialOperations(vec, "+");
-    int parallel_sum = getParallelOperations(vec, "+");
-    ASSERT_EQ(sequential_sum, parallel_sum);
+
+std::vector<int> create_random_graph(const size_t points, const size_t max_distance) {
+    size_t size = points * points;
+    std::mt19937 gen(uint32_t(time(0)));
+    std::uniform_int_distribution<int> uid(-static_cast<int>(max_distance*10),
+                                            static_cast<int>(max_distance));
+    std::vector<int> matrix(size);
+
+    for (size_t i = 0; i < points; i++) {
+        for (size_t j = 0; j < points; j++) {
+            if (i == j) {
+                matrix[i * points + j] = 0;
+            } else {
+                int distance = uid(gen);
+                if (distance <= 0) {
+                    distance = -1;
+                }
+                matrix[i * points + j] = distance;
+                matrix[i + j * points] = distance;
+            }
+        }
+    }
+    return matrix;
 }
 
-TEST(Parallel_Operations_OpenMP, Test_Diff) {
-    std::vector<int> vec = getRandomVector(100);
-    int sequential_diff = getSequentialOperations(vec, "-");
-    int parallel_diff = getParallelOperations(vec, "-");
-    ASSERT_EQ(sequential_diff, parallel_diff);
+TEST(Omp, DISABLED_Test_Time) {
+    const size_t root = 1;
+    const size_t pointsCount = 5000;
+    double time_start, time_end;
+
+    std::vector<int> graph = create_random_graph(pointsCount, 100);
+    // print_graph(graph, pointsCount);
+
+    time_start = omp_get_wtime();
+    auto distances_seq = dijkstra(graph, root, 4800);
+    time_end = omp_get_wtime();
+    std::cout << "Sequential Time: " << time_end - time_start << std::endl;
+
+    time_start = omp_get_wtime();
+    auto distances_parallel = dijkstra_omp(graph, root, 4800);
+    time_end = omp_get_wtime();
+    std::cout << "Parallel Time: " << time_end - time_start << std::endl;
+    ASSERT_EQ(distances_seq, distances_parallel);
 }
 
-TEST(Parallel_Operations_OpenMP, Test_Diff_2) {
-    std::vector<int> vec = getRandomVector(10);
-    int sequential_diff = getSequentialOperations(vec, "-");
-    int parallel_diff = getParallelOperations(vec, "-");
-    ASSERT_EQ(sequential_diff, parallel_diff);
+TEST(Omp, Test_EmptyGraph) {
+    std::vector<int> graph = {};
+
+    ASSERT_ANY_THROW(dijkstra_omp(graph, 0, 0));
 }
 
-TEST(Parallel_Operations_OpenMP, Test_Mult) {
-    std::vector<int> vec = getRandomVector(10);
-    int sequential_mult = getSequentialOperations(vec, "*");
-    int parallel_mult = getParallelOperations(vec, "*");
-    ASSERT_EQ(sequential_mult, parallel_mult);
+TEST(Omp, Test_Graph1) {
+    std::vector<int> graph = { 0 };
+    std::vector<int> result = { 0 };
+
+    ASSERT_EQ(result, dijkstra_omp(graph, 1, 1));
 }
 
-TEST(Parallel_Operations_OpenMP, Test_Mult_2) {
-    std::vector<int> vec = getRandomVector(5);
-    int sequential_mult = getSequentialOperations(vec, "*");
-    int parallel_mult = getParallelOperations(vec, "*");
-    ASSERT_EQ(sequential_mult, parallel_mult);
+TEST(Omp, Test_Graph2) {
+    std::vector<int> graph = { 0, 7,
+                               7, 0 };
+    std::vector<int> result = { 1, 2 };
+
+    ASSERT_EQ(result, dijkstra_omp(graph, 1, 2));
 }
 
-int main(int argc, char **argv) {
+TEST(Omp, Test_Graph6) {
+    std::vector<int> graph = { 0, 7, 9, -1, -1, 14,
+                              7, 0, 10, 15, -1, -1,
+                              9, 10, 0, 11, -1, 2,
+                              -1, 15, 11, 0, 6, -1,
+                              -1, -1, -1, 6, 0, 9,
+                               14, -1, 2, -1, 9, 0 };
+    std::vector<int> result = { 1, 3, 6 };
+
+    print_graph(graph, 6);
+
+    ASSERT_EQ(result, dijkstra_omp(graph, 1, 6));
+}
+
+TEST(Omp, Test_Graph6a) {
+    std::vector<int> graph = { 0, 1, 4, -1, 2, -1,
+                              1, 0, -1, 9, -1, -1,
+                              4, -1, 0, 7, -1, -1,
+                              -1, 9, 7, 0, -1, 2,
+                              2, -1, -1, -1, 0, 8,
+                              -1, -1, -1, 2, 8, 0 };
+    std::vector<int> result = { 1, 5, 6 };
+
+    ASSERT_EQ(result, dijkstra_omp(graph, 1, 6));
+}
+
+TEST(Sequential, Test_EmptyGraph) {
+    std::vector<int> graph = {};
+
+    ASSERT_ANY_THROW(dijkstra(graph, 0, 0));
+}
+
+TEST(Sequential, Test_Graph1) {
+    std::vector<int> graph = {0};
+    std::vector<int> result = {1};
+
+    ASSERT_EQ(result, dijkstra(graph, 1, 1));
+}
+
+TEST(Sequential, Test_Graph2) {
+    std::vector<int> graph = { 0, 7,
+                               7, 0 };
+    std::vector<int> result = {1, 2};
+
+    ASSERT_EQ(result, dijkstra(graph, 1, 2));
+}
+
+TEST(Sequential, Test_Graph6) {
+    std::vector<int> graph = { 0, 7, 9, -1, -1, 14,
+                              7, 0, 10, 15, -1, -1,
+                              9, 10, 0, 11, -1, 2,
+                              -1, 15, 11, 0, 6, -1,
+                              -1, -1, -1, 6, 0, 9,
+                               14, -1, 2, -1, 9, 0 };
+    std::vector<int> result = { 1, 3, 6 };
+
+    print_graph(graph, 6);
+
+    ASSERT_EQ(result, dijkstra(graph, 1, 6));
+}
+
+TEST(Sequential, Test_Graph6a) {
+    std::vector<int> graph = {
+                               0,  1,  4, -1,  2, -1,
+                               1,  0, -1,  9, -1, -1,
+                               4, -1,  0,  7, -1, -1,
+                              -1,  9,  7,  0, -1,  2,
+                               2, -1, -1, -1,  0,  8,
+                              -1, -1, -1,  2,  8,  0 };
+    std::vector<int> result = { 1, 5, 6 };
+
+    print_graph(graph, 6);
+
+    ASSERT_EQ(result, dijkstra(graph, 1, 6));
+}
+
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
