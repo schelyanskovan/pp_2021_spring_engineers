045d46972cd36c14a81fad223ff29fbe1e1f1ded
TheKabeton <TheKabeton@yandex.ru>
2021-05-14 11:01:04 +0300

    tbb

--- modules/task_2/zhafyarov_o_graham_pass_omp/graham_pass.cpp
+++ modules/task_2/zhafyarov_o_graham_pass_omp/graham_pass.cpp
@@ -4,8 +4,6 @@
 #include <random>
 #include <vector>
 
-<<<<<<< HEAD
-=======
 bool CompareVectors(const std::vector<point> &vec1,
                     const std::vector<point> &vec2) {
   if (vec1.size() != vec2.size()) {
@@ -21,20 +19,14 @@ bool CompareVectors(const std::vector<point> &vec1,
   return true;
 }
 
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
 std::vector<point> RandomVector(int size) {
   bool flag = false;
   std::vector<point> vec_tmp(size);
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
   for (int i = 0; i < size; i++) {
-<<<<<<< HEAD
-    vec_tmp[i].x = gen() % 150;
-    vec_tmp[i].y = gen() % 100;
-=======
     vec_tmp[i].x = gen() % 1700 + 100;
     vec_tmp[i].y = gen() % 900 + 100;
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
   }
   while (!flag) {
     for (int i = 0; i < size; i++) {
@@ -43,13 +35,8 @@ std::vector<point> RandomVector(int size) {
           continue;
         }
         if (vec_tmp[i].x == vec_tmp[j].x && vec_tmp[i].y == vec_tmp[j].y) {
-<<<<<<< HEAD
-          vec_tmp[i].x = gen() % 150;
-          vec_tmp[i].y = gen() % 100;
-=======
           vec_tmp[i].x = gen() % 1700 + 100;
           vec_tmp[i].y = gen() % 900 + 100;
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
           i = 0;
           j = 0;
         }
@@ -64,63 +51,8 @@ double Rotation(point a, point b, point c) {
   return((b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x));
 }
 
-<<<<<<< HEAD
-std::vector<size_t> GrahamPassOmp(const std::vector<point>& basis_vec,
-                                  int* count) {
-    std::vector<size_t> result;
-    std::vector<point> result_point;
-
-    int block = static_cast<int>(basis_vec.size()) / omp_get_max_threads();
-    int remainder = static_cast<int>(basis_vec.size()) % omp_get_max_threads();
-
-    if (4*omp_get_max_threads() >= static_cast<int>(basis_vec.size())) {
-        result = GrahamPassSeq(basis_vec, count);
-        return result;
-    }
-
-#pragma omp parallel shared(basis_vec, result, result_point, block, remainder)
-    {
-        int id = omp_get_thread_num();
-        std::vector<point> result_thread;
-        std::vector<size_t> result_tmp;
-
-        if (id == 0) {
-            result_thread.reserve(block + remainder);
-            result_thread.insert(result_thread.begin(), basis_vec.begin(),
-                                 basis_vec.begin() + block + remainder);
-        } else {
-            result_thread.reserve(basis_vec.size() /
-            omp_get_num_threads());
-            result_thread.insert(result_thread.begin(),
-                                 basis_vec.begin() + remainder + id * block,
-                                 basis_vec.begin() + remainder +
-                                 id * block + block);
-        }
-
-#pragma omp barrier
-
-        result_tmp = GrahamPassSeq(result_thread, count);
-
-#pragma omp critical
-        {
-            for (size_t result_from_tmp : result_tmp) {
-                result_point.push_back(result_thread[result_from_tmp]);
-            }
-        }
-}
-    result = GrahamPassSeq(result_point, count);
-    return result;
-}
-
-std::vector<size_t> GrahamPassSeq(const std::vector<point>& basis_vec,
-  int* count) {
-  std::vector<size_t> result;
-
-  if (basis_vec.size() == 1)  return result;
-=======
 std::vector<point> GrahamPassSeq(const std::vector<point>& basis_vec) {
   std::vector<size_t> result_index;
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
 
   std::vector<int> vec_tmp;
 
@@ -128,15 +60,10 @@ std::vector<point> GrahamPassSeq(const std::vector<point>& basis_vec) {
 
   for (int i = 1; i < static_cast<int>(basis_vec.size()); i++) {
     min = basis_vec.at(min).x > basis_vec.at(i).x ||
-<<<<<<< HEAD
-      (basis_vec.at(min).x == basis_vec.at(i).x &&
-        basis_vec.at(min).y > basis_vec.at(i).y) ? i : min;
-=======
           (basis_vec.at(min).x == basis_vec.at(i).x &&
            basis_vec.at(min).y > basis_vec.at(i).y)
           ? i
           : min;
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
   }
 
   vec_tmp.push_back(min);
@@ -150,34 +77,14 @@ std::vector<point> GrahamPassSeq(const std::vector<point>& basis_vec) {
 
   for (int i = 2; i < static_cast<int>(basis_vec.size()); i++) {
     int j = i;
-<<<<<<< HEAD
-    while (j > 1 && Rotation(basis_vec.at(min), basis_vec.at(vec_tmp.at(j - 1)),
-      basis_vec.at(vec_tmp.at(j))) < 0) {
-=======
     while (j > 1 && Rotation(basis_vec.at(min),
                              basis_vec.at(vec_tmp.at(j - 1)),
                              basis_vec.at(vec_tmp.at(j))) < 0) {
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
       std::swap(vec_tmp.at(j), vec_tmp.at(j - 1));
       j--;
     }
   }
 
-<<<<<<< HEAD
-  result.push_back(vec_tmp.at(0));
-  result.push_back(vec_tmp.at(1));
-
-  for (int i = 2; i < static_cast<int>(basis_vec.size()); i++) {
-    while (Rotation(basis_vec.at(result.at(result.size() - 2)),
-      basis_vec.at(result.at(result.size() - 1)),
-      basis_vec.at(vec_tmp.at(i))) < 0) {
-      result.pop_back();
-    }
-    result.push_back(vec_tmp.at(i));
-  }
-
-  *count = static_cast<int>(result.size());
-=======
   result_index.push_back(vec_tmp.at(0));
   result_index.push_back(vec_tmp.at(1));
 
@@ -247,7 +154,6 @@ std::vector<point> GrahamPassOmp(const std::vector<point>& basis_vec) {
   }
 
   result = GrahamPassSeq(result_point);
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
 
   return result;
 }
--- modules/task_2/zhafyarov_o_graham_pass_omp/main.cpp
+++ modules/task_2/zhafyarov_o_graham_pass_omp/main.cpp
@@ -4,58 +4,6 @@
 #include "./graham_pass.h"
 
 TEST(OpenMP, Six_Point_Check_Omp) {
-<<<<<<< HEAD
-  std::vector<point> vec = {{1, 5}, {4, 8}, {8, 2}, {9, 3}, {1, 8}, {7, 7}};
-  std::vector<size_t> result;
-  int count = 0;
-  result = GrahamPassOmp(vec, &count);
-  std::vector<size_t> compare = { 0, 2, 3, 5, 1, 4 };
-  ASSERT_EQ(compare, result);
-}
-
-TEST(OpenMP, Nine_Point_Check_Omp) {
-  std::vector<point> vec = { {1, 5}, {4, 8}, {8, 2}, {9, 3}, {1, 8},
-    {7, 7}, {5, 5}, {2, 1}, {11, 9} };
-  std::vector<size_t> result;
-  int count = 0;
-  result = GrahamPassOmp(vec, &count);
-  std::vector<size_t> compare = { 0, 7, 2, 3, 8, 4 };
-  ASSERT_EQ(compare, result);
-}
-
-TEST(OpenMP, Count_Check_Omp) {
-  std::vector<point> vec = { {1, 1}, {1, 4}, {4, 1}, {4, 4}, {2, 2}};
-  std::vector<size_t> result;
-  int count = 0;
-  int count_tmp = 4;
-  result = GrahamPassOmp(vec, &count);
-  ASSERT_EQ(count, count_tmp);
-}
-
-TEST(OpenMP, Negative_Vector_Check) {
-  ASSERT_ANY_THROW(std::vector<point> vec = RandomVector(-1));
-}
-
-TEST(OpenMP, Test_Time_Seq_And_Omp) {
-  int size = 3000;
-  int count1 = 0;
-  int count2 = 0;
-  std::vector<point> vec = RandomVector(size);
-  std::vector<size_t> result1;
-  std::vector<size_t> result2;
-
-  double start = omp_get_wtime();
-  result1 = GrahamPassOmp(vec, &count1);
-  double end = omp_get_wtime();
-  std::cout << "Omp = " << end - start << std::endl;
-
-  start = omp_get_wtime();
-  result2 = GrahamPassSeq(vec, &count2);
-  end = omp_get_wtime();
-  std::cout << "Seq = " << end - start << std::endl;
-
-  ASSERT_GT(count1, 0);
-=======
   std::vector<point> vec = {{1, 5}, {4, 8}, {8, 2},
                                   {9, 3}, {1, 8}, {7, 7}};
   std::vector<point> result;
@@ -101,7 +49,6 @@ TEST(OpenMP, x300_Points_Omp) {
   std::vector<point> result;
   result = GrahamPassOmp(vec);
   ASSERT_GT(static_cast<int>(result.size()), 0);
->>>>>>> e7f861fbfa7268f39b890d9b1fd24a102758d246
 }
 
 int main(int argc, char **argv) {
