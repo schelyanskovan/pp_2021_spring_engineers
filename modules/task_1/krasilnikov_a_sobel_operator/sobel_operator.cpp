f9a7ac16f06ebfb98ad6424d5014772a8f0c2641
TheKabeton <TheKabeton@yandex.ru>
2021-05-16 21:33:03 +0300

    correction std::size_t

--- modules/task_1/krasilnikov_a_sobel_operator/sobel_operator.cpp
+++ modules/task_1/krasilnikov_a_sobel_operator/sobel_operator.cpp
@@ -6,9 +6,9 @@
 #include <vector>
 
 std::vector<unsigned char> ToMonochrome(
-    const std::vector<unsigned char>& image, size_t height, size_t weight) {
+    const std::vector<unsigned char>& image, std::size_t height, std::size_t weight) {
     std::vector<unsigned char> ans;
-    for (size_t i = 0; i < height * weight * 3; i += 3) {
+    for (std::size_t i = 0; i < height * weight * 3; i += 3) {
         auto pixel = round(
                 std::max(0., std::min(255., 0.2989 * image[i] + 0.5870 *
                                       image[i + 1] + 0.1140 * image[i + 2])));
@@ -18,10 +18,10 @@ std::vector<unsigned char> ToMonochrome(
 }
 
 std::vector<unsigned char> SobelOperator(
-    const std::vector<unsigned char>& image, size_t height, size_t weight) {
+    const std::vector<unsigned char>& image, std::size_t height, std::size_t weight) {
     std::vector<unsigned char> ans(height * weight);
-    for (size_t i = 1; i < height - 1; ++i) {
-        for (size_t j = 1; j < weight - 1; ++j) {
+    for (std::size_t i = 1; i < height - 1; ++i) {
+        for (std::size_t j = 1; j < weight - 1; ++j) {
             auto Gy = image[(i + 1) * height + (j - 1)] -
                       image[(i - 1) * height + (j - 1)] +
                       2 * image[(i + 1) * height + (j)] -
